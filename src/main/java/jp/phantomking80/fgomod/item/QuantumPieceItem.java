ff7f25e255d9680fa313dac3ab19f83a69da8919
PhantomKing80 <phantomking80.modding@gmail.com>
2020-10-26 23:40:24 +0900

    固有情報（アイテムの説明）表示を追加

--- src/main/java/jp/phantomking80/fgomod/item/QuantumPieceItem.java
+++ src/main/java/jp/phantomking80/fgomod/item/QuantumPieceItem.java
@@ -1,6 +1,13 @@
 package jp.phantomking80.fgomod.item;
 
+import java.util.List;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 
 /**
  * QuantumPieceの設定
@@ -17,11 +24,25 @@ public class QuantumPieceItem extends Item {
     this.setRegistryName("quantum_piece");
   }
 
-  // 説明を入れたいときに使う
-  // Shiftを押したときのコメントとか
-  // @Override
-  // public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip,
-  // ITooltipFlag flagIn) {
-  // super.addInformation(stack, worldIn, tooltip, flagIn);
-  // }
+  /**
+   * 固有情報の設定
+   */
+  @Override
+  public void addInformation(ItemStack stack, World worldIn, List<ITextComponent> tooltip,
+      ITooltipFlag flagIn) {
+    super.addInformation(stack, worldIn, tooltip, flagIn);
+
+    // Ctrl押下判定 : func_231172_r_()
+    // Shift押下判定 : func_231173_s_()
+    // Alt押下判定 : func_231174_t_()
+
+    if (Screen.func_231173_s_()) {
+      // Shift押下時
+      // \u00A7e : 黄色
+      tooltip.add(new StringTextComponent("\u00A7e いわゆるQPとかいうやつ"));
+    } else {
+      // デフォルト表示
+      tooltip.add(new StringTextComponent(" Shiftでコメント表示"));
+    }
+  }
 }
